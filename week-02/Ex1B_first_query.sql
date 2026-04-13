43ee9eeb879ba0baea4ab5b8a0e818f9fac6bf15
luis ocelotllopez <locelotllopez@my.yearupunited.org>
2026-04-13 13:57:51 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,9 @@
 /*
 sakill is a data set to collect information on acto and film to organize them and the library they acted in 
 */
+
+select *
+from actor;	-- Retreived 200 Records
+
+select *
+from film; -- Retreived 1000 Records
