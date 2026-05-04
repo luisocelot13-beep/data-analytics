3320f8735bdacbb486d1e2f6cdf7b3637326594c
Luis Lopez <Luisocelot13@gmail.com>
2026-05-04 14:48:10 -0400

    Practice with variables

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,20 @@
 print('Hello world!')
+
+
+messages = ' Hello World'
+print(messages)
+
+# Displaying dollars and cents
+
+dollars = 3 
+cents = .50
+
+print(dollars + cents)
+
+# the print functions added the values within the variables and came to 3.5
+cents = cents + .25
+print(dollars + cents)
+
+d_string = '3 dollars'
+c_str = '50 cents'
+print(d_string + " " + c_str)
