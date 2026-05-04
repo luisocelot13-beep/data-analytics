4881f01e7b001173a827afc0e7f1eb2e7f245b08
Luis Lopez <Luisocelot13@gmail.com>
2026-05-04 15:53:27 -0400

    completed flexiple ,python

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,27 @@
+Customer_id = 1 # it'll be the first id so 1
+customer_name = "Luis" # customer name will be there first name and last name needs there own variable
+Customer_gender = "male" # gender of customer
+Birth_date = '02-14-1998' # the day they wer born
+Driver_License_number = '' # we dont have there drivers license number yet so its empty.
+
+Full_name = ' Luis Ocelotl'
+City_birth = 'Districto federal'
+State_birth = 'Mexico_country'
+
+City_grew_up = 'winston salem'
+State_grew_up = 'North carolina'
+
+# a) What is the full list of reserved words that can’t be used for variable names?
+ # False,def,if,raise,None,del,import,return,True,elif,in,try,and,else,is,while,as,except lamba
+ # with, assert,finally nonlocal,yield,break,for,not,class,form,or,continue global pass.
+
+# b) Pick 5 of these words and review the explanation for how it is used as a keyword in
+# Python. Add these 5 definitions as # comments to your exercise document. Put ^^
+#  around any terms that you are not familiar with.
+
+# def: The def function is used to define a function or a method in Python.
+# if: an if statement is used to make a conditional statement. if the condition is True,then some action is performed.
+# raise: The raise statement is used to raise an error. These errors are visible in the traceback and they cancel the execution of the program if not handled properly.
+# del: the del statement is used to delete an object in python.
+
+# import: The statement is used to import modules to the project.
