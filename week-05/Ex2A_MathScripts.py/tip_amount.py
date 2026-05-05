b3c6c8c855bba942c3d1b95e23e23df4b1944890
luis Ocelotl <Luisocelot13@gmail.com>
2026-05-04 22:22:30 -0400

    introducing input to tip amount formula

--- week-05/Ex2A_MathScripts.py/tip_amount.py
+++ week-05/Ex2A_MathScripts.py/tip_amount.py
@@ -1,5 +1,5 @@
 # tip amount
-bill = 80
+bill = int(input('Type your bill amount:'))
 tip_percent = 0.20
 
 tip = bill * tip_percent
@@ -7,3 +7,6 @@ tip = bill * tip_percent
 total_bill = tip + bill
 
 print ('The Tip on a $' + str(bill)  + ' resturant bill is $' + format(tip,'.2f'))
+
+#introducing the input function to tip amount 
+# the possible pit falls i can see is mabye customer might write out the number and it might be issues when python runs it
