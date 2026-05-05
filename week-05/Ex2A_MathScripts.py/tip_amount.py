aa5d9569a016801c56a666de97dfb4e3d8e62016
luis Ocelotl <Luisocelot13@gmail.com>
2026-05-04 21:34:46 -0400

    tip amount formula

---
+++ week-05/Ex2A_MathScripts.py/tip_amount.py
@@ -0,0 +1,9 @@
+# tip amount
+bill = 80
+tip_percent = 0.20
+
+tip = bill * tip_percent
+
+total_bill = tip + bill
+
+print ('The Tip on a $' + str(bill)  + ' resturant bill is $' + format(tip,'.2f'))
