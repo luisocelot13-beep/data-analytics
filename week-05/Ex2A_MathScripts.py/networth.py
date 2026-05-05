575957e387823317b4393feeeb482bfe49150c6c
luis Ocelotl <Luisocelot13@gmail.com>
2026-05-04 20:49:14 -0400

    building networth formula

---
+++ week-05/Ex2A_MathScripts.py/networth.py
@@ -0,0 +1,25 @@
+#1. How do you calculate your net worth given your assets and debts?
+# assets are things you own that have value like cash,savings account,cars,property
+# Debts is what you owe to others like credit card,loans,morgages
+
+# Assets
+cash = 5000
+investments = 12000
+car = 8000
+
+# Debts 
+credit_cards = 2000
+stundet_loan = 7000
+
+# calculations
+total_assets = cash + investments + car
+total_debt = credit_cards + stundet_loan
+
+# Networth
+net_worth = total_assets - total_debt
+
+# print statements
+print('Your total assets are ' ,total_assets)
+print('Your total debts are' , total_debt)
+print('Your net worth is' , net_worth)
+
