f279430757d3b4c44103acc51d1af98a96b4db09
luis Ocelotl <Luisocelot13@gmail.com>
2026-05-06 18:49:37 -0400

    updates

--- week-05/Ex3B_SampleCollections/candy_store.py
+++ week-05/Ex3B_SampleCollections/candy_store.py
@@ -2,11 +2,11 @@ fruit_flavors = ('apple','kiwi','watermelon')
 
 fruity_flavors = ('lemon','sunny','kite')
 
-candy_combinations = set()
+candy_combinations = set() # empty container 
 
 candy_combinations.add( fruit_flavors[0] + ' ' + fruity_flavors[0]) 
-candy_combinations.add(fruit_flavors[1] + ' ' + fruity_flavors[1])
+candy_combinations.add(fruit_flavors[1] + ' ' + fruity_flavors[1])    # candy put into pairs
 candy_combinations.add(fruit_flavors[2] + ' '  +  fruity_flavors [2])
 print('Todays candy options include:')
 print(candy_combinations)
-# what i noticed immediately is that the output would change its patter im assuming set has to be the reason
+# what i noticed immediately is that the output would change its patter im assuming set has to be the reason.
--- week-05/Ex3B_SampleCollections/movie_list.py
+++ week-05/Ex3B_SampleCollections/movie_list.py
@@ -5,4 +5,4 @@ print(sorted(list_name)) # the sorted sets them alphabetically
 print(list_name) # unsorted just shows them the way i originally typed them in
 
 list_name.sort() # i get error doing it this way
-print(list_name) # just the way i submitted it
+print(list_name) # just the way i submitted it.
