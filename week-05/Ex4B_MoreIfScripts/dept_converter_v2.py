f9af63d90431292a669b5b047a3aeadbaacbf9e1
luis Ocelotl <Luisocelot13@gmail.com>
2026-05-06 20:22:57 -0400

    completed dep converter v2

---
+++ week-05/Ex4B_MoreIfScripts/dept_converter_v2.py
@@ -0,0 +1,31 @@
+Departments ={1: 'Marketing',
+              5:'Human Resources',
+              10:'Accounting', 
+              12:'Legal', 
+              18: 'IT',
+              20:'Customer Relations'}
+
+department_search = int(input('Type in dept code:'))
+
+match department_search:
+
+    case 1:
+        print(f'Your department is {Departments[1]}')
+
+    case 5:
+        print(f'Your department is {Departments[5]}')
+    
+    case 10:
+        print(f'Your department is {Departments[10]}')
+    
+    case 12:
+        print(f'Your departmen is {Departments[12]}')
+
+    case 18:
+        print(f'Your department is {Departments[18]}')
+
+    case 20:
+        print(f'Your department is {Departments[20]}')
+
+    case _:
+        print('Cant find your department')
