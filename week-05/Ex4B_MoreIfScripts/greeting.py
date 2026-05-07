32340ea9e1027cd4a36b80e4278a835a65588d02
luis Ocelotl <Luisocelot13@gmail.com>
2026-05-06 20:37:29 -0400

    completed greeting

---
+++ week-05/Ex4B_MoreIfScripts/greeting.py
@@ -0,0 +1,13 @@
+current_hour = 5
+
+if current_hour >= 4 and current_hour <= 11:
+    print('Why you up so late')
+
+elif current_hour < 10:
+    print('Goodmorning')
+
+elif current_hour < 17:
+    print('Good day')
+
+else:
+    print('Good eveneing')
